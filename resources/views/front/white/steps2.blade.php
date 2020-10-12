cf30866fa233d2cfc1d89322b68f76c049827470
Frank <frank.mancina@mindgeek.com>
2020-10-12 17:32:43 -0400

    Update steps2.blade.php
    check for logo image, if not show header

--- resources/views/front/white/steps2.blade.php
+++ resources/views/front/white/steps2.blade.php
@@ -6,7 +6,7 @@
                         <path d="M15.932 18.649L.466 2.543A1.35 1.35 0 0 1 0 1.505c0-.41.155-.77.466-1.081A1.412 1.412 0 0 1 1.504 0c.41 0 .756.141 1.038.424l16.992 17.165c.31.283.466.636.466 1.06 0 .423-.155.777-.466 1.06L2.542 36.872a1.412 1.412 0 0 1-1.038.424c-.41 0-.755-.141-1.038-.424A1.373 1.373 0 0 1 0 35.813c0-.423.155-.776.466-1.059L15.932 18.65z" fill="#726D75" fill-rule="evenodd"/>
                 </svg>
             </span>
-            <a href="//contests.withlovedarling.com" title="">CONTESTS</a>
+            <a href="/" title="">KONKURRENCER</a>
         </small>
     </nav>
 </div>
@@ -29,7 +29,7 @@
         <p class="t-font-bold t-my-3 t-text-xl md:t-text-2xl lg:t-text-3xl xl:t-text-4xl">{{ $contest->slogan}}</p>
         <p></p>
         <hr class="t-m-auto t-my-2 t-w-4/5 lg:t-w-3/5 t-border t-border-gray-400" />
-        <p class="t-text-lg md:t-text-2xl">Here's how</p>
+        <p class="t-text-lg md:t-text-2xl">Sådan gør du</p>
         <p class="t-font-bold t-text-4xl lg:t-text-6xl t-m-0 xl:t-mt-6">1</p>
         <p class="t-font-bold t-text-2xl sm:t-text-4xl t-m-0">{{ $contest->step1_title}}</p>
         <p class="t-text-base sm:t-text-lg t-m-0">{!! $contest->step1_body !!}</p>
@@ -43,10 +43,10 @@
         <p class="t-text-base sm:t-text-lg t-m-0">{!! $contest->step3_body !!}</p>
         <br />
         <hr class="t-m-auto t-my-2 t-w-4/5 lg:t-w-3/5 t-border t-border-gray-400" />
-        <p class="t-font-bold t-text-2xl sm:t-text-4xl">Enter by {{ $contest->getSubmitDateText() }}</p>
+        <p class="t-font-bold t-text-2xl sm:t-text-4xl">Indsend inden den {{ $contest->getSubmitDateText() }}</p>
         <p class="t-text-base sm:t-text-lg">
             <a class="btn btn--large btn--secondary btn--neutral caps" href="/enter-contest">
-                Enter Contest
+                Indtast Konkurrence
             </a>
         </p>
     </div>
