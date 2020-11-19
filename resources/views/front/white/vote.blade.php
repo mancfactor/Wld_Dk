bde9ba094ebd4854427c22617034d6c7eed94dfe
Frank <frank.mancina@mindgeek.com>
2020-11-19 14:49:55 -0500

    Update vote.blade.php
    pink background

--- resources/views/front/white/vote.blade.php
+++ resources/views/front/white/vote.blade.php
@@ -1,6 +1,6 @@
 <h2 class="theme coll-heading">STEM P&Aring; DIN FAVORIT</h2>
 <div id="shopify-section-index-vote" class="shopify-section">             
-    <div id="stepsrow">
+    <div id="stepsrow" class="wldpink t-antialiased t-text-gray-900">
         <p class="t-text-center t-p-4 sm:t-p-10 t-text-base sm:t-text-lg" >
             <a href="/featured" class="btn btn--large btn--secondary btn--neutral caps">STEM NU</a>
         </p>
